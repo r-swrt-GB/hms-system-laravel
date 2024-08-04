8f9037a6a6f6654c7b0b18e6a5b45bcb3db2b3af
Theunis Kok <>
2024-08-04 14:08:28 +0200

    Created ASSIGNMENTS table, USER_ON_SUBMISSION table, and SUBMISSION table

---
+++ database/migrations/2024_08_04_112104_create_assignments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('min_videos')->default(0);
+            $table->integer('max_videos')->default(0);
+            $table->integer('max_video_length')->default(0);
+            $table->integer('max_grade')->default(0);
+            $table->dateTime('open_date')->nullable();
+            $table->dateTime('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
