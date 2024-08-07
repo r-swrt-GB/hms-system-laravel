72e8b4892183f2a6ed171fa6bf3ca07b995b52ee
Rikus Swart <rikus.swart@greenbit.dev>
2024-08-07 18:24:32 +0200

    Defined relationship between Roles, Rights, and User

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Role
+ *
+ * @property int $id
+ * @property string $role_name
+ * @property string $slug
+ * @property string $description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'role_name',
+        'slug',
+        'description'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('User', 'user_role');
+    }
+
+    public function rights()
+    {
+        return $this->belongsToMany('Right','role_on_right');
+    }
+
 }
