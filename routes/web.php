4b7c5301f707b50a66c81c12524c7f7294454a21
Rikus Swart <rikus.swart@greenbit.dev>
2024-08-12 08:59:45 +0200

    Added default navigation routes

--- routes/web.php
+++ routes/web.php
@@ -49,4 +49,9 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profile
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 require __DIR__ . '/auth.php';
