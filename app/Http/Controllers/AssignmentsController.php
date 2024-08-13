00d4a98f70f2474d07d7ddd74be635714f265ae5
Henk-Mooiman <hendrikwillemmooiman@gmail.com>
2024-08-13 23:16:59 +0200

    Updated Assignment Controller Routes

--- app/Http/Controllers/AssignmentsController.php
+++ app/Http/Controllers/AssignmentsController.php
@@ -8,14 +8,18 @@ use Inertia\Inertia;
 
 class AssignmentsController extends Controller
 {
-    public function index(Request $request) {
-        return Inertia::render('Assignments/AssignmentsPage');}
+    public function index(Request $request)
+    {
+        return Inertia::render('Assignments/AssignmentsPage');
+    }
+
     public function getAssignmentPage(Request $request, Assignment $assignment)
     {
-        return Inertia::render('AssignmentPage');
+        return Inertia::render('Assignments/AssignmentPage');
     }
+
     public function getCreateAssignmentPage(Request $request)
     {
-        return Inertia::render('CreateAssignmentsPage');
+        return Inertia::render('Assignments/CreateAssignmentsPage');
     }
 }
